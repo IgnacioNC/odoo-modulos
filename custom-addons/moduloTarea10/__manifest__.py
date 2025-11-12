ebb87de24afec5b0f54e0325864f80ad6949511f
IgnacioNC <dedalin1968@hotmail.com>
2025-11-12 15:05:37 +0000

    Inicialización de nuevo modulo moduloTarea10

---
+++ custom-addons/moduloTarea10/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "moduloTarea10",
+
+    'summary': "Módulo tarea 10",
+
+    'description': """
+Descripción del módulo de la tarea 10
+    """,
+
+    'author': "My Company",
+    'website': "127.0.0.1:8069",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Inventory',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
+
