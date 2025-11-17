6ba4716185934ff1a66c3a2fe6faf5ada5c2f40d
IgnacioNC <44934490+IgnacioNC@users.noreply.github.com>
2025-11-17 17:42:10 +0100

    Se han agregado la ruta de los permisos de acceso.

--- custom-addons/moduloTarea10/__manifest__.py
+++ custom-addons/moduloTarea10/__manifest__.py
@@ -22,7 +22,8 @@ Descripción del módulo de la tarea 10
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
