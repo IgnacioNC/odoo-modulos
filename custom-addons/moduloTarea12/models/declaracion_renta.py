d37ab048017385dbc25a9f13c982cba04eae6ecf
IgnacioNC <44934490+IgnacioNC@users.noreply.github.com>
2025-12-03 16:01:35 +0100

    Limite máximo de nominas en la declaració de la renta.

--- custom-addons/moduloTarea12/models/declaracion_renta.py
+++ custom-addons/moduloTarea12/models/declaracion_renta.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class DeclaracionRenta(models.Model):
     _name = "empleado.renta"
@@ -9,6 +10,8 @@ class DeclaracionRenta(models.Model):
 
     nominas_ids = fields.Many2many("nomina.empleado", string="Nóminas")
 
+    max_nominas = 14
+
     sueldo_bruto_total = fields.Float(
         compute="_compute_totales", store=True
     )
@@ -18,6 +21,13 @@ class DeclaracionRenta(models.Model):
 
     @api.depends("nominas_ids")
     def _compute_totales(self):
-        for rec in self:
-            rec.sueldo_bruto_total = sum(n.sueldo_base + n.total_bonificaciones for n in rec.nominas_ids)
-            rec.impuestos_pagados = sum(n.irpf_pagado for n in rec.nominas_ids)
+        for record in self:
+            record.sueldo_bruto_total = sum(n.sueldo_base + n.total_bonificaciones for n in record.nominas_ids)
+            record.impuestos_pagados = sum(n.irpf_pagado for n in record.nominas_ids)
+
+    @api.constrains("nominas_ids")
+    def _check_max_nominas(self):
+        for record in self:
+            if len(record.nominas_ids) > record.max_nominas:
+                raise ValidationError("Solo se permiten un máximo de 3 nóminas vinculadas.")
+
