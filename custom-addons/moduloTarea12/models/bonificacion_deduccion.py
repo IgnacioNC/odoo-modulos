4e6e4fbc6fea00c43791497556dfab1f7aa37519
IgnacioNC <44934490+IgnacioNC@users.noreply.github.com>
2025-12-09 18:42:24 +0100

    a

--- custom-addons/moduloTarea12/models/bonificacion_deduccion.py
+++ custom-addons/moduloTarea12/models/bonificacion_deduccion.py
@@ -6,10 +6,6 @@ class NominaBonificacion(models.Model):
 
     nomina_id = fields.Many2one("nomina.empleado", string="Nómina", required=True)
 
-    importe = fields.Float(
-        string="Importe Bruto",
-        help="Positivo = bonificación, negativo = deducción",
-        required=True
-    )
+    importe = fields.Float(string="Importe Bruto", help="Positivo = bonificación, negativo = deducción", required=True)
 
     concepto = fields.Char(string="Concepto", required=True)
